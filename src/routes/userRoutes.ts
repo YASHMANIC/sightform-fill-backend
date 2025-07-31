8a91ffc98c6cc89e6433f1885d847921e1f51aeb
YASHMANIC <yashmanic96@gmail.com>
2025-07-31 15:07:51 +0530

    final commit

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -75,7 +75,7 @@ router.post('/login', async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production', // set to true in production with HTTPS
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
